4144ab73333a381d3dc8a985837125c37c545e4b
ljordan51 <ljordan51@gmail.com>
2017-03-15 13:50:38 -0400

    fixed main loop to handle input args from command line, adding more detailed commenting

--- counter.py
+++ counter.py
@@ -30,24 +30,32 @@ def update_counter(file_name, reset=False):
     >>> update_counter('blah2.txt')
     2
     """
-    count = 0
+    count = 0  # default count is 0, this way all files will be incremented by 1
+    # if the file doesn't exist, it will create one and dump the default count in
     if not exists(file_name):
         fin = open(file_name, 'wb')
         dump(count, fin)
         fin.close()
+    # if reset is false, the function must first read and load the current counter value
     if not reset:
         fin = open(file_name, 'rb')
         count = load(fin)
         fin.close()
+    # in all cases, the counter value is now ready to be incremented by 1 and dumped back into the file
     fout = open(file_name, 'wb')
     dump(count+1, fout)
     fout.close()
-    return count+1
+    return count+1  # returns the current count for the file
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2:  # if there are no input args then the doctest is run
         import doctest
         doctest.testmod(verbose=True)
     else:
-        print("new value is " + str(update_counter(sys.argv[1])))
+        file_name = sys.argv[1]
+        reset = False  # by default reset is false
+        if len(sys.argv) == 3:
+            if sys.argv[2].lower() == 'true':  # if second arg input string is true then reset is changed to True
+                reset = True
+        print("new value is " + str(update_counter(file_name, reset)))
